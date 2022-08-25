7e051ed4ddb14af017bd69598a8056bd6ce582a7
Vivian Imbriotis <vivian.imbriotis@gmail.com>
2022-08-25 15:51:36 +1000

    pre-refactor

--- data_generation.R
+++ data_generation.R
@@ -15,42 +15,49 @@
 #' @param samples_per_neuron number of samples per neuron
 #'
 #' @return a dataframe with the columns (group, neuron_id, dependant), where each row is an observation
-gen_unpaired_data <- function(control_group_mean=0, treatment_effect=1, control_group_interneuron_sd = 0.5, 
-                             intervention_group_interneuron_sd=0.5, within_neuron_sd=0.5, n_control_neurons=5,
-                             n_intervention_neurons=5, samples_per_neuron=100) {
+#' 
 
+HALF_UNIT_VARIANCE_SD <- sqrt(2)/2
+
+
+gen_unpaired_data <- function(control_group_mean=0, treatment_effect=1, control_group_interneuron_sd = 2, 
+                                   intervention_group_interneuron_sd=2, within_neuron_sd=2, 
+                                   n_control_neurons=10, n_intervention_neurons=10, samples_per_neuron=100) {
+  
+
+  #Making some basic calculations explicit
   intervention_group_mean <- control_group_mean + treatment_effect
+  n_total_neurons <- n_control_neurons + n_intervention_neurons
   
-  #Set up an empty (correctly sized) dataframe to hold our control group data
-  rows = n_control_neurons * samples_per_neuron
-  dat_control <- data.frame(neuron_id = numeric(rows), group=character(rows),dependant=numeric(rows))
+  #Generate control group neuron means about 0, repeat each mean for all the samples
+  control_grp_neuron_means <- rnorm(n_control_neurons, mean=0, sd = control_group_interneuron_sd)
+  neuron_effect <- rep(control_grp_neuron_means, each = samples_per_neuron)
   
-  #For each neuron...
-  for (i in 1:n_control_neurons){
-    #How far away from the group (control or intervention) mean is this neuron's mean?
-    neuron_deviation <- rnorm(1, mean=0, sd = control_group_interneuron_sd)
-    #Randomly generate all that neuron's observations...
-    row_range = seq((i-1)*samples_per_neuron + 1, i*samples_per_neuron)
-    dat_control[row_range,"dependant"] <- rnorm(samples_per_neuron, mean = control_group_mean + neuron_deviation, sd = within_neuron_sd)
-    
-    #Fill out the neuron's exogenous variables
-    dat_control[row_range,"neuron_id"] <- i
-    dat_control[row_range,"group"] <- "Control"
-  }
+  #All the neurons have the same within-neuron variance, so it suffices to add a residual noise term
+  sample_effect <- rnorm(n_control_neurons*samples_per_neuron, mean=0, sd = within_neuron_sd)
+  
+  #Then the samples are the sum of the group mean, the neuron's mean, and the sample effect
+  control_group_samples <- neuron_effect + sample_effect + control_group_mean
+  
   
   #Repeat for the intervention group
-  rows = n_intervention_neurons * samples_per_neuron
-  dat_intervention <- data.frame(neuron_id = numeric(rows), group=character(rows),dependant=numeric(rows))
-  for (i in 1:n_intervention_neurons){
-    neuron_deviation <- rnorm(1, mean=0, sd = intervention_group_interneuron_sd)
-    row_range = seq((i-1)*samples_per_neuron + 1, i*samples_per_neuron)
-    dat_intervention[row_range,"dependant"] <- rnorm(samples_per_neuron, mean = intervention_group_mean + neuron_deviation, sd = within_neuron_sd)
-    dat_intervention[row_range,"neuron_id"] <- i
-    dat_intervention[row_range,"group"] <- "Intervention"
-  }
+  intervention_grp_neuron_means <- rnorm(n_intervention_neurons, mean=0, sd = intervention_group_interneuron_sd)
+  neuron_effect <- rep(intervention_grp_neuron_means, each = samples_per_neuron)
+  sample_effect <- rnorm(n_intervention_neurons*samples_per_neuron, mean=0, sd = within_neuron_sd)
   
-  #Glue the control and intervention groups together and return them
-  dat <- rbind(dat_control,dat_intervention)
+  intervention_group_samples <- neuron_effect + sample_effect + intervention_group_mean
+  
+  #Glue control and intervention groups together
+  all_samples <- c(control_group_samples, intervention_group_samples)
+  
+  #Add the independant vars
+  neuron_id <- rep(1:n_total_neurons, each = samples_per_neuron)
+  neuron_id <- as.factor(neuron_id)
+  
+  group <- c(rep("Control",n_control_neurons*samples_per_neuron),rep("Intervention",n_intervention_neurons*samples_per_neuron))
+  
+  #Return as dataframe
+  dat <- data.frame(neuron_id = neuron_id, group = group, dependant = all_samples)
   
   return(dat)
 }
@@ -60,8 +67,32 @@ plot_data <- function(dat){
   #Plot each neuron's empirical PDF (unfilled histogram) colored by that neuron's group
   plt <- ggplot(data=dat, aes(x = dependant, color = group))
   for (id in unique(dat$neuron_id)){
-    plt <- plt + geom_freqpoly(data = dat[dat$neuron_id==id,])
+    plt <- plt + geom_freqpoly(data = dat[dat$neuron_id==id,], binwidth = 0.2)
   }
   
   return(plt)
 }
+
+
+is_ks_test_positive <- function(dat, alpha = 0.05){
+  #Perform the KS test
+  res <- ks.test(dat$dependant[dat$group=="Control"],dat$dependant[dat$group=="Intervention"])
+  ks_positive <- (res$p.value < alpha)
+  return(ks_positive)
+}
+
+
+is_lmer_test_positive <- function(dat, alpha = 0.05, suppress = FALSE){
+  #Create a mixed model, then create a mixed model with a dropped term, then compare
+  if (suppress){
+    return(suppressMessages(is_lmer_test_positive(dat,alpha)))
+  }
+  model_h1 <- lme4::lmer(dependant ~ group + (1|neuron_id), data = dat, REML = FALSE)
+  model_h0 <- lme4::lmer(dependant ~ (1|neuron_id),         data = dat, REML = FALSE)
+  
+  res <- anova(model_h1, model_h0)
+  #get the p-value and compare to alpha
+  lmer_positive <- (res$`Pr(>Chisq)`[2] < alpha)
+  return(lmer_positive)
+}
+
---
+++ ks_test_heatmap.R
@@ -0,0 +1,7 @@
+source("data_generation.R")
+
+
+
+
+
+dat <- gen_unpaired_data()
